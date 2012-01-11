7201801783ad910ec0bc208228d7b76aeafdee09
Rob@KLPD <rob@dnpa.nl>
2012-01-11 15:08:25 +0100

    Typo fixed. TODO updated

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fakeroot dpkg-deb --build dynr-web dynr-web_`grep Version dynr-web/DEBIAN/control |sed -e 's/.* //'`_`grep Architecture dynr-pbrouting/DEBIAN/control |sed -e 's/.* //'`.deb
+fakeroot dpkg-deb --build dynr-web dynr-web_`grep Version dynr-web/DEBIAN/control |sed -e 's/.* //'`_`grep Architecture dynr-web/DEBIAN/control |sed -e 's/.* //'`.deb
